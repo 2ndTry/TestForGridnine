d9d6bb16a1716389c1de0205c4b707a62f782a2a
Alexey Miroshnikov <mirnijalex@gmail.com>
2020-12-20 14:15:37 +0300

    Test task for Gridnine

---
+++ src/com/gridnine/testing/Filterable.java
@@ -0,0 +1,10 @@
+package com.gridnine.testing;
+
+import java.util.List;
+
+public interface Filterable {
+
+    void ruleOfDepartureToCurrentMoment(List<Flight> flightList);
+    void ruleOfSegmentsArrivalDateEarlier(List<Flight> flightList);
+    void ruleOfTimeOnEarth(List<Flight> flightList);
+}
